8bd9ee97e5d15e46009496dfd0fb32837098ea7f
Nicoleakama <akamanicole48@gmail.com>
2024-04-23 01:15:01 +0300

    hire hub

---
+++ app/build/tmp/kapt3/stubs/debug/net/ezra/ui/auth/SignupKt.java
@@ -0,0 +1,30 @@
+package net.ezra.ui.auth;
+
+import androidx.compose.foundation.text.KeyboardOptions;
+import androidx.compose.material3.*;
+import androidx.compose.runtime.*;
+import androidx.compose.ui.Modifier;
+import androidx.compose.ui.text.input.ImeAction;
+import androidx.compose.ui.text.input.KeyboardCapitalization;
+import androidx.compose.ui.text.input.KeyboardType;
+import androidx.compose.ui.text.input.PasswordVisualTransformation;
+import androidx.compose.ui.text.style.TextAlign;
+import androidx.compose.ui.tooling.preview.Preview;
+import androidx.constraintlayout.compose.Dimension;
+import androidx.navigation.NavHostController;
+import net.ezra.R;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 2, d1 = {"\u0000\u0010\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\u001a\u0010\u0010\u0000\u001a\u00020\u00012\u0006\u0010\u0002\u001a\u00020\u0003H\u0007\u001a\b\u0010\u0004\u001a\u00020\u0001H\u0007\u00a8\u0006\u0005"}, d2 = {"SignupScreen", "", "navController", "Landroidx/navigation/NavHostController;", "SignupScreenPreviewLight", "app_debug"})
+public final class SignupKt {
+    
+    @androidx.compose.runtime.Composable
+    @kotlin.OptIn(markerClass = {androidx.compose.material3.ExperimentalMaterial3Api.class})
+    public static final void SignupScreen(@org.jetbrains.annotations.NotNull
+    androidx.navigation.NavHostController navController) {
+    }
+    
+    @androidx.compose.runtime.Composable
+    @androidx.compose.ui.tooling.preview.Preview(showBackground = true, uiMode = android.content.res.Configuration.UI_MODE_NIGHT_NO)
+    public static final void SignupScreenPreviewLight() {
+    }
+}
